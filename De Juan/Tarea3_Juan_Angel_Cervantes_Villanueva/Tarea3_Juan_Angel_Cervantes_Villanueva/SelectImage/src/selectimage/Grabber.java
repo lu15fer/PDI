0da80282927bf60cbbe061e7524c77f47845d643
Juanzassu <banano_zassu@hotmail.com>
2019-04-10 20:36:41 -0500

    Va con el 4 prros

--- De Juan/Tarea3_Juan_Angel_Cervantes_Villanueva/Tarea3_Juan_Angel_Cervantes_Villanueva/SelectImage/src/selectimage/Grabber.java
+++ De Juan/Tarea3_Juan_Angel_Cervantes_Villanueva/Tarea3_Juan_Angel_Cervantes_Villanueva/SelectImage/src/selectimage/Grabber.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package selectimage;
+package pdi_proyecto4;
 
 import java.awt.Image;
 import java.awt.Toolkit;
